d19f84ac6a68c2b8a870d1364360b38155ea3356
GleamBringer <devastation-bringer@proton.me>
2026-03-03 15:52:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const TARGET = 'http://fsg-01.eagler.host';
 const proxy = httpProxy.createProxyServer({
     target: TARGET,
     ws: true,
-    changeOrigin: true
+    changeOrigin: true,
+    secure: false,
+    followRedirects: true,
+    xfwd: true
 });
 
 const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('FSG MC Proxy is Active');
+        return;
+    }
+
     proxy.web(req, res, (err) => {
         res.writeHead(502);
         res.end();
@@ -17,7 +26,9 @@ const server = http.createServer((req, res) => {
 });
 
 server.on('upgrade', (req, socket, head) => {
-    proxy.ws(req, socket, head);
+    proxy.ws(req, socket, head, (err) => {
+        socket.destroy();
+    });
 });
 
 const PORT = process.env.PORT || 10000;
